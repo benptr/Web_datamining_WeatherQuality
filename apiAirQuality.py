cb868d09329b537a12b33e31293d0ec0e98c8f0c
benptr <benjamin.porterie@edu.devinci.fr>
2022-03-16 19:16:23 +0100

    api management

---
+++ apiAirQuality.py
@@ -0,0 +1,19 @@
+import requests, json
+import pandas as pd
+def getAirQuality(city="paris"):
+    try:
+        url = requests.get("https://api.waqi.info/feed/{}/?token=49a66ce7af17ad7c0c0cb12b3a75cea9930f5634".format(city))
+        airQualityData = json.loads(url.text)
+        print(airQualityData)
+    except:
+        print("Invalid query, The city selected must not exist in the api Data Base")
+    return airQualityData
+
+def CountryData(country="France"):
+    cities = pd.read_csv('https://pkgstore.datahub.io/core/world-cities/world-cities_csv/data/6cc66692f0e82b18216a48443b6b95da/world-cities_csv.csv')
+    country_city = cities[cities['country']==country]['name']
+    result = []
+    for elmt in country_city:
+        result.append(getAirQuality(elmt))
+    return result
+print(CountryData())
