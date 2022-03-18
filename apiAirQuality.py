8a2390b1b94ac5fe765c8b1259812f6cdc6556a1
benptr <benjamin.porterie@edu.devinci.fr>
2022-03-18 18:41:15 +0100

    all data

--- apiAirQuality.py
+++ apiAirQuality.py
@@ -1,10 +1,18 @@
 import requests, json
 import pandas as pd
+
 def getAirQuality(city="paris"):
     try:
         url = requests.get("https://api.waqi.info/feed/{}/?token=49a66ce7af17ad7c0c0cb12b3a75cea9930f5634".format(city))
         airQualityData = json.loads(url.text)
-        print(airQualityData)
+    except:
+        print("Invalid query, The city selected must not exist in the api Data Base")
+    return airQualityData
+
+def getWeather(city="Paris"):
+    try:
+        url = requests.get("https://api.openweathermap.org/data/2.5/weather?q={}&appid=ecab1e85cf8afd4829a1ea59134e3cf5".format(city))
+        airQualityData = json.loads(url.text)
     except:
         print("Invalid query, The city selected must not exist in the api Data Base")
     return airQualityData
@@ -12,8 +20,8 @@ def getAirQuality(city="paris"):
 def CountryData(country="France"):
     cities = pd.read_csv('https://pkgstore.datahub.io/core/world-cities/world-cities_csv/data/6cc66692f0e82b18216a48443b6b95da/world-cities_csv.csv')
     country_city = cities[cities['country']==country]['name']
-    result = []
+    result = {}
     for elmt in country_city:
-        result.append(getAirQuality(elmt))
+        result[elmt] = {"airQ":getAirQuality(elmt),"weather":getWeather(elmt)}
+        print(result[elmt])
     return result
-print(CountryData())
